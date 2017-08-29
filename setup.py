bf05e8207e510dd0be6ddd530adea4873f726a5e
Arthur Chang <arthurc.iphone@gmail.com>
2017-08-29 20:59:15 +0800

    Add setup.py and boyfriend

---
+++ setup.py
@@ -0,0 +1,16 @@
+import boyfriend
+
+from setuptools import setup
+
+
+setup(
+    name='boyfriend',
+    version=boyfriend.__version__,
+    description='Boyfriend package for fun',
+    url='http://github.com/storborg/funniest',
+    author='Arthur Chang',
+    author_email='arthurc0102@gmail.com',
+    license='MIT',
+    packages=['boyfriend'],
+    zip_safe=True
+)
