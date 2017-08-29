386fbadc4e70a51bc734e74b143c5ac96376889f
Arthur Chang <arthurc.iphone@gmail.com>
2017-08-29 21:30:23 +0800

    Random choice text

--- boyfriend/boyfriend.py
+++ boyfriend/boyfriend.py
@@ -1,2 +1,16 @@
+import random
+
 def say():
-    print('親愛的今天過得好嗎？')
+    text = [
+        '親愛的今天過得好嗎？',
+        '吃飽了嗎？',
+        '天氣冷多穿點唷～'
+    ]
+    print(random.choice(text))
+
+def main():
+    say()
+
+
+if __name__ == '__main__':
+    main()
