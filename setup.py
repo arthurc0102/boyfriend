fca993b5595fd89b38b9e6a256b6faf6537e12bc
Arthur Chang <arthurc.iphone@gmail.com>
2017-08-29 21:25:53 +0800

    Update url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='boyfriend',
     version=boyfriend.__version__,
     description='Boyfriend package for fun',
-    url='http://github.com/storborg/funniest',
+    url='https://github.com/arthurc0102/boyfriend',
     author='Arthur Chang',
     author_email='arthurc0102@gmail.com',
     license='MIT',
